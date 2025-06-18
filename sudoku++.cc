dfa721829bcdd68651c2d5132eec7dbe005b7e90
MuriloMonteiroPaesOliveira <murilo.paes@unifesp.br>
2025-06-18 15:13:48 -0300

    Update sudoku++.cc

--- sudoku++.cc
+++ sudoku++.cc
@@ -1,27 +1,25 @@
 #include <stdio.h>
 
-int sudoku[10][10];
+int sudoku[9][9];
 
 int main() {
-    int n = 1; // 1 instância para este exemplo
-    int valido = 1;
-
     FILE *f = fopen("input3.txt", "r");
     if (!f) {
         printf("Erro ao abrir input3.txt\n");
         return 1;
     }
 
-    for (int i=0; i<10; i++) {
-        for (int j=0; j<10; j++) {
+    for (int i=0; i<9; i++) {
+        for (int j=0; j<9; j++) {
             fscanf(f, "%d", &sudoku[i][j]);
         }
     }
     fclose(f);
 
-    for (int i=0; i<10 && valido; i++) {
+    int valido = 1;
+    for (int i=0; i<9 && valido; i++) {
         int linha[11] = {0};
-        for (int j=0; j<10; j++) {
+        for (int j=0; j<9; j++) {
             int val = sudoku[i][j];
             if (val < 1 || val > 10 || linha[val]) {
                 valido = 0;
@@ -31,9 +29,9 @@ int main() {
         }
     }
 
-    for (int j=0; j<10 && valido; j++) {
+    for (int j=0; j<9 && valido; j++) {
         int coluna[11] = {0};
-        for (int i=0; i<10; i++) {
+        for (int i=0; i<9; i++) {
             int val = sudoku[i][j];
             if (val < 1 || val > 10 || coluna[val]) {
                 valido = 0;
@@ -43,12 +41,11 @@ int main() {
         }
     }
 
-    // blocos 2x5
-    for (int bi=0; bi<10 && valido; bi+=2) {
-        for (int bj=0; bj<10 && valido; bj+=5) {
+    for (int bi=0; bi<9 && valido; bi+=3) {
+        for (int bj=0; bj<9 && valido; bj+=3) {
             int bloco[11] = {0};
-            for (int i=bi; i<bi+2; i++) {
-                for (int j=bj; j<bj+5; j++) {
+            for (int i=bi; i<bi+3; i++) {
+                for (int j=bj; j<bj+3; j++) {
                     int val = sudoku[i][j];
                     if (val < 1 || val > 10 || bloco[val]) {
                         valido = 0;
