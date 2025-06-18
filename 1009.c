40973ba490c0c7630e688ab318dc2f3ab954d640
MuriloMonteiroPaesOliveira <murilo.paes@unifesp.br>
2025-06-18 14:54:51 -0300

    Create 1009.c

---
+++ 1009.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    char nome[50];
+    double salarioFixo, totalVendas, totalReceber;
+    scanf("%s", nome);
+    scanf("%lf", &salarioFixo);
+    scanf("%lf", &totalVendas);
+    totalReceber = salarioFixo + (totalVendas * 0.15);
+    printf("TOTAL = R$ %.2lf\n", totalReceber);
+    return 0;
+}
